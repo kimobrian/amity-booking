996bcb06f8671e91ad6837b4197741a4dc1a6e64
kimobrian <brian.kimokoti@andela.com>
2016-09-23 15:19:50 +0300

    (chore) class Amity tests

--- application/amity.py
+++ application/amity.py
@@ -1,30 +1,26 @@
 class Amity(object):
-	"""Amity facility"""
+    '''Amity class'''
 
-	def print_allocations(self, output_file):
-    	"""
-    	Prints all room allocations.
-		Outputs to file if filename is specified
-    	"""
+    def print_allocations(self, filename=None):
         pass
 
     def save_state(self, db_name):
-    	"""
-		Save current state of operations to database
-    	"""
+        """
+                Save current state of operations to database
+        """
         pass
 
     def load_state(self, db_name):
-    	"""
-    	Loads Amity infromation from database
-    	"""
+        """
+        Loads Amity infromation from database
+        """
         pass
 
-    def print_unallocated(self, output_file):
-    	"""
-		Prints unallocated people. 
-		Outputs to file if filenam eis provided
-    	"""
+    def print_unallocated(self, output_file=None):
+        """
+                Prints unallocated people. 
+                Outputs to file if filenam eis provided
+        """
         pass
 
     def __str__(self):
---
+++ application/tests/test_amity.py
@@ -0,0 +1,28 @@
+from nose.tools import assert_equal
+from application.amity import Amity
+
+
+class TestAmity(object):
+    '''
+    Tests for amity class
+    '''
+
+    def test_print_allocations(self):
+        amity = Amity()
+        assert_equal(amity.print_allocations(), True,
+                     'Function did not return True')
+
+    def test_save_state(self):
+        amity = Amity()
+        assert_equal(amity.save_state('db_name'),
+                     True, 'Saving state to DB failed')
+
+    def test_load_state(self):
+        amity = Amity()
+        assert_equal(amity.load_state('db_name'), True,
+                     'Loading state from DB failed')
+
+    def test_print_unallocated(self):
+        amity = Amity()
+        assert_equal(amity.print_unallocated('file_name'), True,
+                     'Failed to print unallocated people to file')
